ecd980a11eb5b0b0011e05cfd7bbddd866e356ed
ThomasKGilbert <thomaskentgilbert@icloud.com>
2026-09-03 00:12:03 -0400

    Implemented frontend ui + some js

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,40 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
+const inputText = document.getElementById("task-text");
+const taskListContainer = document.getElementById("task-list-container");
 
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+function addTask() {
+  if(inputText.value === "") {
+    alert("Please enter a task");
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputText.value;
+    taskListContainer.appendChild(li);
+  }
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
-
-  const text = await response.text()
-
-  console.log( 'text:', text )
 }
-
-window.onload = function() {
-  const button = document.querySelector('button')
-  button.onclick = submit
-}
+// const submit = async function( event ) {
+//   // stop form submission from trying to load
+//   // a new .html page for displaying results...
+//   // this was the original browser behavior and still
+//   // remains to this day
+//   event.preventDefault()
+//
+//   const input = document.querySelector( '#yourname' ),
+//         json = { yourname: input.value },
+//         body = JSON.stringify( json )
+//
+//   const response = await fetch( '/submit', {
+//     method:'POST',
+//     body
+//   })
+//
+//   const text = await response.text()
+//
+//   console.log( 'text:', text )
+// }
+//
+// window.onload = function() {
+//   const button = document.querySelector('button')
+//   button.onclick = submit
+// }
